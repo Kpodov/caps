4a91379d56003ad210d5435b7ce7b305875bb0aa
Kpodovzky <josuk@pm.me>
2019-11-10 19:57:14 -0500

    Update pull-setup.sh

--- pull-setup.sh
+++ pull-setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt-get -y install rsync
 sudo cp rsyncd.conf /etc/rsyncd.conf
+sudo rm /var/run/rsyncd.pid
 sudo rsync --daemon
 mkdir -p /home/$USER/logs/health
 sudo cp rsync-pull.sh /root/
