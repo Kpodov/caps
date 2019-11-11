1a71f3c30c33116cbea7d30afd774f69dd6dbad6
Kpodovzky <josuk@pm.me>
2019-11-10 19:23:02 -0500

    Update pull-setup.sh

--- pull-setup.sh
+++ pull-setup.sh
@@ -2,7 +2,9 @@
 sudo apt-get -y install rsync
 sudo cp rsyncd.conf /etc/rsyncd.conf
 sudo rsync --daemon
+mkdir -p /home/$USER/logs/health
 # Change the IP address to lure's and change 'health' to the lure type
 rsync -rtu --delete rsync://ubuntu@3.133.213.19/logs ~/logs/health
 chmod +x rsync-pull.sh
 sudo cp rsync-pull.sh /root/
+sudo crontab -u root /root/rsync-config
