8eb67ea66d960302d7314c6a822c44387b8157b2
Kpodovzky <josuk@pm.me>
2019-11-10 20:09:32 -0500

    Update pull-setup.sh

--- pull-setup.sh
+++ pull-setup.sh
@@ -3,6 +3,7 @@ sudo apt-get -y install rsync
 sudo cp rsyncd.conf /etc/rsyncd.conf
 sudo rm /var/run/rsyncd.pid
 sudo rsync --daemon
+sudo rsync -rtu --delete rsync://ubuntu@3.133.213.19/logs ~/logs/health/
 mkdir -p /home/$USER/logs/health
 sudo cp rsync-pull.sh /root/
 sudo crontab -u root rsync-config
