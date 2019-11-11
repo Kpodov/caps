6c731626037f04648355dd2cb1b7523f7f8c59eb
Kpodovzky <josuk@pm.me>
2019-11-10 19:55:41 -0500

    Update pull-setup.sh

--- pull-setup.sh
+++ pull-setup.sh
@@ -4,4 +4,4 @@ sudo cp rsyncd.conf /etc/rsyncd.conf
 sudo rsync --daemon
 mkdir -p /home/$USER/logs/health
 sudo cp rsync-pull.sh /root/
-sudo crontab -u root /root/rsync-config
+sudo crontab -u root rsync-config
