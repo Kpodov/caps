35f690f7ee4f5a194d6da3346e68e2d63cc0ea68
Kpodovzky <josuk@pm.me>
2019-11-10 21:23:53 -0500

    Update rsync-pull.sh

--- rsync-pull.sh
+++ rsync-pull.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Change the IP address to lure's and change 'health' to the lure type
-rsync -rtu --delete rsync://ubuntu@3.133.213.19/logs ~/logs/health
+rsync -arvRtu --delete rsync://ubuntu@3.133.213.19/logs ~/logs/health
